5f5ab27852f0cf3e0f25689438f8c411ec6f44dd
mauricio.silva <mauricio.silva@akqa.com>
2011-01-19 11:24:39 -0800

    further clean up of room names and descr layout

--- public/rooms.js
+++ public/rooms.js
@@ -1,135 +1,135 @@
 var rooms = [
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "ca",
 	"name": "Castro",
-	"desc": "King Street side",
+	"desc": "King Street Side",
 	"img" : "6thFloor_h.png"
 },
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "ch",
 	"name": "Chinatown",
-	"desc": "Mid Building",
+	"desc": "Mid Building East Side",
 	"img" : "6thFloor_h.png"
 },
 {
 	"floor": "LL2",
 	"key": "dp",
 	"name": "Dog Patch",
-	"desc": "Townsend side",
+	"desc": "Townsend Side",
 	"img" : "LL2_h.png"
 },
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "ha",
 	"name": "Haight",
-	"desc": "Townsend side",
+	"desc": "Townsend Side",
 	"img" : "6thFloor_h.png"
 },
 {
-	"floor": "5",
+	"floor": "5th",
 	"key": "hv",
 	"name": "Hayes Valley",
-	"desc": "King Street side",
+	"desc": "King Street Side",
 	"img" : "5thFloor_h.png"
 },
 {
-  "floor":4,
+  "floor":"4th",
   "key": "hpr",
   "name":"Health Partners Team Room",
-  "desc":"King Street side",
+  "desc":"King Street Side",
 	"img" : "4thFloor_h.png"
 },
 {
-	"floor": "4t",
+	"floor": "4th",
 	"key": "ma",
 	"name": "Marina",
-	"desc": "Townsend side",
+	"desc": "Townsend Side",
 	"img" : "4thFloor_h.png"
 },
 {
 	"floor": "LL2",
 	"key": "ms",
 	"name": "Mission",
-	"desc": "Townsend side",
+	"desc": "Townsend Side",
 	"img" : "LL2_h.png"
 },
 {
-  "floor":4,
+  "floor":"4th",
   "key": "ntr",
   "name": "Nike Team Room",
-  "desc": "Townsend side",
+  "desc": "Townsend Side",
 	"img" : "4thFloor_h.png"
 },
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "nh",
 	"name": "Nob Hill",
-	"desc": "Mid Building",
+	"desc": "Mid Building West Side",
 	"img" : "6thFloor_h.png"
 },
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "nb",
 	"name": "North Beach",
-	"desc": "Mid Building",
+	"desc": "Mid Building West Side",
 	"img" : "6thFloor_h.png"
 },
 {
-	"floor": "5",
+	"floor": "5th",
 	"key": "pa",
 	"name": "Pacific Heights",
 	"desc": "Townsend Side",
 	"img" : "5thFloor_h.png"
 },
 {
-	"floor": "4",
+	"floor": "4th",
 	"key": "ph",
 	"name": "Potrero Hill",
-	"desc": "Townsend side",
+	"desc": "Townsend Side",
 	"img" : "4thFloor_h.png"
 },
 {
-	"floor": "5",
+	"floor": "5th",
 	"key": "rh",
 	"name": "Russian Hill",
-	"desc": "Mid Building",
+	"desc": "Mid Building West Side",
 	"img" : "5thFloor_h.png"
 },
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "so",
 	"name": "Soma",
-	"desc": "King Street side",
+	"desc": "King Street Side",
 	"img" : "6thFloor_h.png"
 },
 {
-	"floor": "6",
+	"floor": "6th",
 	"key": "su",
 	"name": "Sunset",
-	"desc": "Townsend side",
+	"desc": "Townsend Side",
 	"img" : "6thFloor_h.png"
 },
 {
-  "floor": 5,
+  "floor": "5th",
   "key": "vcr",
   "name": "Visa Creative War Room",
-  "desc": "Mid Building 2nd Street side",
+  "desc": "Mid Building East Side",
 	"img" : "5thFloor_h.png"
 },
 {
-  "floor": 5,
+  "floor": "5th",
   "key": "vtr",
   "name": "Visa Technology War Room",
-  "desc": "Mid Building 3rd Street side",
+  "desc": "Mid Building West Side",
 	"img" : "5thFloor_h.png"
 },
 {
-  "floor":4,
+  "floor":"4th",
   "key": "xb",
   "name": "XBox Team Room",
-  "desc": "King Street side",
+  "desc": "King Street Side",
 	"img" : "4thFloor_h.png"
 }
 ]
